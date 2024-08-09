d98478c391e9e1c10fa68bfa3fa2a7ff77f5acd5
Henrique Gabriel Siqueira da Cruz <henriquepro8@gmail.com>
2024-08-09 10:00:50 -0300

    forgot password

--- screens/ForgotPassword.js
+++ screens/ForgotPassword.js
@@ -1,19 +1,66 @@
-import { View, Text, SafeAreaView, StyleSheet, Image, TextInput, Button, Pressable, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, StyleSheet, Image, TextInput, Button, Pressable, TouchableOpacity, ActivityIndicator } from 'react-native'
+import React, {useState} from 'react'
 import { LinearGradient } from 'expo-linear-gradient';
 import logo from '../assets/logos.png'
 import email from '../assets/email3.png'
 import LoginButton from '../components/LoginButton';
 import { useNavigation } from '@react-navigation/native';
+import Toast from 'react-native-root-toast';
+import axios from 'axios';
 
 const ForgotPassword = () => {
 
   const navigation = useNavigation();
+  const [email, setEmail] = useState('');
+  const [loading, setLoading] = useState(false)
+  const [response, setResponse] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null)
+  
+  const handleForgotPassword = async () => {
+    if(email.length < 2){
+      Toast.show("O email deve estar preenchido");
+      return;
+    }
+    setLoading(true);
 
-  const [text, onChangeText] = React.useState('Email');
+    try {
+
+      const credentials = {
+        email: email,
+      }
+
+      const res = await axios.post(global.apiUrl + '/api/user/forgot_password', credentials).then(function (res_api) {
+        
+        setResponse(res_api.data);
+        Toast.show(res_api.data.message);
+        navigation.navigate('Login')
+
+
+      }).catch(function (error) {
+        
+        Toast.show(error.response.data.message)
+        
+      });
+
+    } catch (error) {
+      setLoading(false);
+      console.error('Erro ao fazer a requisição POST:', error);
+    }
+
+    setLoading(false);
+  }
 
   const login = () => {
         navigation.navigate('Login')
+  }
+
+  if (loading) {
+    // Exibe um indicador de carregamento enquanto espera a resposta da API
+    return (
+      <View className="flex-1 justify-center items-center">
+        <ActivityIndicator size="large" color="green" />
+      </View>
+    );
   }
 
   return (
@@ -39,7 +86,7 @@ const ForgotPassword = () => {
               <View className="w-2 justify-center items-center">
                 
               </View>
-              <TextInput placeholder="Email" maxLength={40} className="w-64 h-10 p-1"></TextInput>
+              <TextInput value={email} onChangeText={email => setEmail(email)} placeholder="Email" maxLength={60} className="w-64 h-10 p-1"></TextInput>
             </View>
 
             
@@ -50,15 +97,10 @@ const ForgotPassword = () => {
             
 
           </View>
-          <View className="flex-2 w-80 items-end pr-6">
-            <View className="flex-row rounded-sm pb-8">
-              
-              
-            </View>
-          </View>
+          
           <View>
-            <View className="flex-row rounded-sm">
-                <LoginButton title="Enviar"></LoginButton>
+            <View className="flex-row rounded-sm mt-6">
+                <LoginButton press={handleForgotPassword} title="Enviar"></LoginButton>
                 
             </View>
           </View>
