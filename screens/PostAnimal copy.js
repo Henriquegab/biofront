2ed0f822f86cb7e93df4c2f152d4f72909088636
Henrique Gabriel Siqueira da Cruz <henriquepro8@gmail.com>
2024-08-20 19:02:09 -0300

    post animal

---
+++ screens/PostAnimal copy.js
@@ -0,0 +1,157 @@
+import React, { useState } from 'react';
+import { View, TextInput, Button, Image, Text, StyleSheet, ScrollView, SafeAreaView } from 'react-native';
+import { useForm, Controller } from 'react-hook-form';
+import * as ImagePicker from 'expo-image-picker';
+import CampoText from '../components/CampoText';
+import CardComponent from '../components/CardComponent';
+import Toast from 'react-native-root-toast';
+import axios from 'axios';
+import CampoModal from '../components/CampoModal';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const PostAnimal = () => {
+  
+  const [photo, setPhoto] = useState(null);
+
+  const [campo1, setCampo1] = useState('');
+  const [campo2, setCampo2] = useState('');
+  const [campo3, setCampo3] = useState('');
+  const [mostrarModal, setmostrarModal] = useState(false);
+  const [response, setResponse] = useState("");
+
+  const { control, handleSubmit, formState: { errors } } = useForm();
+
+  async function onSubmit() {
+    if(campo1 == "" || campo2 == "" || campo3 == "" || photo == null){
+      Toast.show("Preencha todos os campos!")
+    }
+    else{
+      setmostrarModal(true);
+
+      const token = await AsyncStorage.getItem('token');
+      try {
+        const payload = {
+          titulo: campo1,
+          animal: campo2,
+          descricao: campo3,
+        }
+
+        const res = await axios.post(global.apiUrl + '/api/animal', payload, {headers: {'Authorization': `Bearer ${token}`}}).then(function (res_api){
+          setResponse(res_api.data)
+          Toast.show(res_api.data.message)
+          setmostrarModal(false);
+
+        }).catch(function (error){
+          Toast.show(error.response.data.message)
+          setmostrarModal(false);
+        })
+
+
+      } catch (error) {
+        setmostrarModal(false);
+        Toast.show(error.message)
+      }
+      // requisição para api ou salvamento no celular
+    }
+  };
+
+  const selectPhoto = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setPhoto(result.assets[0].uri);
+    }
+  };
+
+  const takePhoto = async () => {
+    let result = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setPhoto(result.assets[0].uri);
+    }
+  };
+
+  return (
+
+    <SafeAreaView className="bg-zinc-900 flex-1">
+
+    
+    <ScrollView contentContainerStyle={styles.container}>
+
+        <CardComponent>
+            <View className="mb-4 items-center">
+                {photo && <Image source={{ uri: photo }} style={styles.photo} />}
+
+            </View>
+                    
+
+            <View className="flex-row space-x-6 justify-center">
+                <View>
+
+                    <Button title="Selecionar Foto" onPress={selectPhoto} />
+                </View>
+                <View>
+                    <Button title="Tirar Foto" onPress={takePhoto} />
+                </View>
+                
+            </View>
+
+
+            <CampoText mudarValor={(value) => setCampo1(value)} placeholder="Título"></CampoText>
+            <CampoText mudarValor={(value) => setCampo2(value)} placeholder="Animal"></CampoText>
+            <CampoText mudarValor={(value) => setCampo3(value)} placeholder="Descrição" text={true}></CampoText>
+
+            <View>
+                <Button className="" title="Cadastrar" onPress={handleSubmit(onSubmit)} /> 
+            </View>
+
+        </CardComponent>
+
+      
+
+      <CampoModal mostrarModal={mostrarModal}></CampoModal>
+    </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    padding: 10,
+    justifyContent: 'start',
+    alignItems: 'center',
+  },
+  label: {
+    fontSize: 16,
+    marginVertical: 4,
+  },
+  input: {
+    width: '100%',
+    padding: 8,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 4,
+    marginBottom: 8,
+  },
+  error: {
+    color: 'red',
+    marginBottom: 8,
+  },
+  photo: {
+    width: 200,
+    height: 150,
+    marginTop: 16,
+  },
+});
+
+export default PostAnimal;
--- screens/PostAnimal.js
+++ screens/PostAnimal.js
@@ -1,157 +1,51 @@
-import React, { useState } from 'react';
-import { View, TextInput, Button, Image, Text, StyleSheet, ScrollView, SafeAreaView } from 'react-native';
-import { useForm, Controller } from 'react-hook-form';
-import * as ImagePicker from 'expo-image-picker';
-import CampoText from '../components/CampoText';
-import CardComponent from '../components/CardComponent';
-import Toast from 'react-native-root-toast';
-import axios from 'axios';
-import CampoModal from '../components/CampoModal';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import React from 'react';
+import { View, Text, TextInput, TouchableOpacity, SafeAreaView, ScrollView } from 'react-native';
 
-const PostAnimal = () => {
-  
-  const [photo, setPhoto] = useState(null);
-
-  const [campo1, setCampo1] = useState('');
-  const [campo2, setCampo2] = useState('');
-  const [campo3, setCampo3] = useState('');
-  const [mostrarModal, setmostrarModal] = useState(false);
-  const [response, setResponse] = useState("");
-
-  const { control, handleSubmit, formState: { errors } } = useForm();
-
-  async function onSubmit() {
-    if(campo1 == "" || campo2 == "" || campo3 == "" || photo == null){
-      Toast.show("Preencha todos os campos!")
-    }
-    else{
-      setmostrarModal(true);
-
-      const token = await AsyncStorage.getItem('token');
-      try {
-        const payload = {
-          titulo: campo1,
-          animal: campo2,
-          descricao: campo3,
-        }
-
-        const res = await axios.post(global.apiUrl + '/api/animal', payload, {headers: {'Authorization': `Bearer ${token}`}}).then(function (res_api){
-          setResponse(res_api.data)
-          Toast.show(res_api.data.message)
-          setmostrarModal(false);
-
-        }).catch(function (error){
-          Toast.show(error.response.data.message)
-          setmostrarModal(false);
-        })
-
-
-      } catch (error) {
-        setmostrarModal(false);
-        Toast.show(error.message)
-      }
-      // requisição para api ou salvamento no celular
-    }
-  };
-
-  const selectPhoto = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
-
-    if (!result.canceled) {
-      setPhoto(result.assets[0].uri);
-    }
-  };
-
-  const takePhoto = async () => {
-    let result = await ImagePicker.launchCameraAsync({
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
-
-    if (!result.canceled) {
-      setPhoto(result.assets[0].uri);
-    }
-  };
-
+const AdScreen = () => {
   return (
+    <SafeAreaView className="flex-1 bg-zinc-900">
+      <ScrollView className="">
+        {/* <View className="flex-row items-center justify-between p-4">
+          <TouchableOpacity>
+            <Text className="text-white">Voltar</Text>
+          </TouchableOpacity>
+          <Text className="text-white">LIMPAR</Text>
+        </View> */}
 
-    <SafeAreaView>
+        <View className="items-center justify-center h-3/5">
+          <TouchableOpacity className="bg-bioVerde rounded-lg p-4">
+            <Text className="text-white font-bold">Incluir fotos</Text>
+          </TouchableOpacity>
+          <Text className="text-gray-400 mt-2">0 de 20 adicionadas</Text>
+        </View>
 
-    
-    <ScrollView contentContainerStyle={styles.container}>
+        <View className="bg-bioBrancoPrincipal rounded-t-3xl px-4 py-6">
+          <Text className="font-bold text-gray-800">Título do anúncio*</Text>
+          <TextInput
+            className="border border-gray-300 rounded-lg px-4 py-2 mt-2"
+            placeholder="Ex: Samsung S9 novo na caixa"
+          />
 
-        <CardComponent>
-            <View className="mb-4 items-center">
-                {photo && <Image source={{ uri: photo }} style={styles.photo} />}
+          <Text className="font-bold text-gray-800 mt-4">Descrição*</Text>
+          <TextInput
+            className="border border-gray-300 rounded-lg px-4 py-2 mt-2 h-24"
+            multiline
+            placeholder="Ex: Smartphone Samsung Galaxy S9 com 128gb de memória, com caixa, todos os cabos e sem marca de uso."
+          />
 
-            </View>
-                    
+          <Text className="font-bold text-gray-800 mt-4">Marca*</Text>
+          <TextInput
+            className="border border-gray-300 rounded-lg px-4 py-2 mt-2"
+            placeholder="Ex: Samsung"
+          />
 
-            <View className="flex-row space-x-6 justify-center">
-                <View>
-
-                    <Button title="Selecionar Foto" onPress={selectPhoto} />
-                </View>
-                <View>
-                    <Button title="Tirar Foto" onPress={takePhoto} />
-                </View>
-                
-            </View>
-
-
-            <CampoText mudarValor={(value) => setCampo1(value)} placeholder="Título"></CampoText>
-            <CampoText mudarValor={(value) => setCampo2(value)} placeholder="Animal"></CampoText>
-            <CampoText mudarValor={(value) => setCampo3(value)} placeholder="Descrição" text={true}></CampoText>
-
-            <View>
-                <Button className="" title="Cadastrar" onPress={handleSubmit(onSubmit)} /> 
-            </View>
-
-        </CardComponent>
-
-      
-
-      <CampoModal mostrarModal={mostrarModal}></CampoModal>
-    </ScrollView>
+          <TouchableOpacity className="bg-bioVerde rounded-lg p-4 mt-6">
+            <Text className="text-white font-bold">Anunciar</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flexGrow: 1,
-    padding: 10,
-    justifyContent: 'start',
-    alignItems: 'center',
-  },
-  label: {
-    fontSize: 16,
-    marginVertical: 4,
-  },
-  input: {
-    width: '100%',
-    padding: 8,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 4,
-    marginBottom: 8,
-  },
-  error: {
-    color: 'red',
-    marginBottom: 8,
-  },
-  photo: {
-    width: 200,
-    height: 150,
-    marginTop: 16,
-  },
-});
-
-export default PostAnimal;
+export default AdScreen;
